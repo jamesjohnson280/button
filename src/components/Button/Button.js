99a99ebb888d04cbcd6fd1089e5ff6a22d3da1f8
James Johnson <james.johnson280@gmail.com>
2020-09-25 07:59:01 -0400

    Fix syntax

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './Button.css';
 
 function Button(props) {
-  const label = props.label || props.children;
+  const label = props.label || props.children
   return (
     <button onClick={props.onClick} >{label}</button>
   );
